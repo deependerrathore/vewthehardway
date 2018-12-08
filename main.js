687858f9658b0337bfc5f3e3e60012f2c53439be
Deepender Rathore <deepenrathore28@gmail.com>
2018-12-08 15:44:06 +1100

    bind element attributes

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
+var app1 = new Vue({
+    el:"#app1",
+    data:{
+        message: "You have loaded this mage on " + new Date().toLocaleString()
+    }
+})
 var app = new Vue({
     el:"#app",
     data:{
         message: "Hello Vue"
     }
-});
+})
 
 setTimeout(function(){
     app.message = "Fooobaaar"
 }, 2000);
+
