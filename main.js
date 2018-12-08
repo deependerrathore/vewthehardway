74a244cf39a39d1e913138721c1ac4b6ec18f1f1
Deepender Rathore <deepenrathore28@gmail.com>
2018-12-08 17:04:08 +1100

    Vue js reactive

--- main.js
+++ main.js
@@ -7,11 +7,19 @@ var app1 = new Vue({
 var app = new Vue({
     el:"#app",
     data:{
-        message: "Hello Vue"
+        message: "Hello Vue",
+        name:{
+            first: "n/a",
+            last: "n/a"
+        }
     }
 })
 
-setTimeout(function(){
-    app.message = "Fooobaaar"
-}, 2000);
 
+setTimeout(() => app.message = "Updated Hello Vue", 1000);
+setTimeout(() => app.name = {first:"Deepender",last:"Rathore"},2000);
+setTimeout(() => app.name.first = "Narender",3000);
+//setTimeout(() => app.name.initials = "NS",3000); //this will not work as we don't have any property in data
+//Even if we have above problem then also it will not work because when we are setting but app.name on line 20 initials will lost as we are not setting anyting there
+
+setTimeout(() => Vue.set(app.name,'initials','NS'),4000);
